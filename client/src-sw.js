87f7ae9efe621abae6fe30238e687dbaaa681a5c
Mike Sanders <msanders331@gmail.com>
2022-05-07 13:35:14 -0700

    added eventlistener to the install button. Updated the SRC is updated. updated plugins and added babel.

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,17 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+//setting up the asset cache
+registerRoute(
+  ({request}) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+
+    chaceName: 'asset-cache',
+    plugins[
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ]
+  })
+
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,41 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //plugin that generates our html file and injects the bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDes0t: '.src-sw.js'
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
